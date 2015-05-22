5069e183344bd7138c0a192d62777a7217ea42d0
Jelle Herold <jelle@defekt.nl>
2015-05-23 00:30:59 +0200

    include aggregations in result

--- src/elasticsearch.js
+++ src/elasticsearch.js
@@ -10,18 +10,76 @@ var client = new elasticsearch.Client({
   log: logLevel
 });
 
+var _ = require('lodash');
+
+var aggs = {
+    "denomination": {
+      "terms": {
+        "field": "denomination",
+        "size": 50
+      }
+    },
+    "building_type": {
+      "terms": {
+        "field": "building_type",
+        "size": 10
+      }
+    },
+    "method": {
+      "terms": {
+        "field": "method",
+        "size": 10
+      }
+    },
+    "category": {
+      "terms": {
+        "field": "category",
+        "size": 10
+      }
+    },
+    "population": {
+      "terms": {
+        "field": "population",
+        "size": 10
+      }
+    },
+    "kind_of_education": {
+      "terms": {
+        "field": "kind_of_education",
+        "size": 10
+      }
+    },
+    "strict_or_free": {
+      "terms": {
+        "field": "strict_or_free",
+        "size": 10
+      }
+    }
+  };
+
 exports.schoolQuery = function(q){
 	return client
 		.search({
 			index: index,
 			type: 'school',
 			body: {
-				query: q
+				query: q,
+				aggs: aggs
 			}
 		})
 		.then(function(results){
 			// return list of source documents
-			return results.hits.hits.map(function(x){return x._source;});
-		})
-;
+			return {
+				total: results.hits.total,
+				matches: results.hits.hits.map(function(x){return x._source;}),
+				facets: _(results.aggregations).mapValues(function(val) {
+					return val.buckets.map(function(v){
+						return {
+							value: v.key,
+							count: v.doc_count
+						}
+					});
+				})
+			};
+		});
 }
